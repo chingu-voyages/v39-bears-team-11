903f0fa82dea6968f0185ac806b14329bee45e43
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-21 14:19:24 +0100

    :sparkles: Enforce eslint rules

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,13 +1,17 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
+import webVitals from 'web-vitals'
 
-export default reportWebVitals;
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    webVitals.then(({
+      getCLS, getFID, getFCP, getLCP, getTTFB,
+    }) => {
+      getCLS(onPerfEntry)
+      getFID(onPerfEntry)
+      getFCP(onPerfEntry)
+      getLCP(onPerfEntry)
+      getTTFB(onPerfEntry)
+    })
+  }
+}
+
+export default reportWebVitals
