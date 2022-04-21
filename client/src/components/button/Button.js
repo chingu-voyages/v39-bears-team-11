698e4bc1972766cd2cf19e5b1fb24f4e53cd54fd
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-21 13:02:19 +0100

    :construction: Import login and logout actions

--- client/src/components/button/Button.js
+++ client/src/components/button/Button.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../../styles/Button.module.css'
 import { useSelector, useDispatch } from 'react-redux'
+import { login, logout } from '../../store/features/users/usersSlice'
 
 function Button() {
   return (
