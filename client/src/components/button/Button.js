aa52d2200a5ae9466228dbcef9ed24f833c75cb1
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-21 13:32:40 +0100

    :arrow_up: Update Button

--- client/src/components/button/Button.js
+++ client/src/components/button/Button.js
@@ -2,20 +2,25 @@ import React, { Fragment } from 'react'
 import styles from '../../styles/Button.module.css'
 import { useSelector, useDispatch } from 'react-redux'
 import { login, logout } from '../../store/features/users/usersSlice'
+import { nanoid } from '@reduxjs/toolkit';
 
 function Button() {
-  const users = useSelector(state => state.users.value);
+  const users = useSelector(state => state.users);
   const dispatch = useDispatch();
 
   const handleLogin = () => {
-
+    dispatch(login({
+      id: nanoid(),
+      name: "Moody Mudiaga",
+    }));
   }
 
   const handleLogout = () => {
-
+    dispatch(logout());
   }
 
   return <Fragment>
+      <h1>Welcome, {users.name}</h1>
       <button 
       className={styles.Button}
       onClick={handleLogin}
