1b5834e5df1bd9fc14803f7d835a98b9d8cfba42
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-21 14:06:34 +0100

    :sparkles: Enforce eslint rules

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,21 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable semi */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store/store';
-import { Provider } from 'react-redux';
 
+// eslint-disable-next-line no-undef
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
