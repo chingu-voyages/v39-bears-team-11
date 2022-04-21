72ccb6de02b508907434f9897340b82db80aad73
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-21 13:04:19 +0100

    :construction: Initialize users from redux store

--- client/src/components/button/Button.js
+++ client/src/components/button/Button.js
@@ -4,6 +4,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { login, logout } from '../../store/features/users/usersSlice'
 
 function Button() {
+  const users = useSelector(state => state.users.value);
+
   return (
     <button className={styles.Button}>Button</button>
   )
