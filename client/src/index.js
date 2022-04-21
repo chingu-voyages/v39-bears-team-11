9fc954f53c3f86bec9e3a6e981f21b64e7c659ec
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-21 08:38:39 +0100

    :construction: Provide redux store to App

--- client/src/index.js
+++ client/src/index.js
@@ -8,10 +8,12 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
