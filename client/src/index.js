a5830869dd43f6b65d4e14e3ab8c4a2fb64f8450
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-21 08:37:24 +0100

    :construction: Import redux Provider component

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
