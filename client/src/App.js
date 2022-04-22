7d9b05417c36d0b9f45deab017e9aaadec086a35
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-04-22 18:26:39 +0100

    Add Router and main routes to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,44 @@
 import React from 'react'
-import Button from './components/button/Button'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import styles from './styles/App.module.css'
+import Signup from './components/signup/Signup'
+import Chats from './components/chats/Chats'
+import ChatRoom from './components/chatroom/ChatRoom'
+import Search from './components/search/Search'
+import Profile from './components/profile/Profile'
+import Friends from './components/friends/Friends'
 
 function App() {
   return (
     <div className="App">
       <header className={styles.App} />
-      <Button />
+
+      <Router>
+        <Routes>
+          <Route exact path="/">
+            <Signup />
+          </Route>
+          <Route path="signup">
+            <Signup />
+          </Route>
+          <Route path="/chats">
+            <Chats />
+          </Route>
+          <Route path="/chatroom">
+            <ChatRoom />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/friends">
+            <Friends />
+          </Route>
+        </Routes>
+      </Router>
+
     </div>
   )
 }
