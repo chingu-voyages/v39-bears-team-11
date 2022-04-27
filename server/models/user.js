ac3c82dbe00d714c73b7f63764714a3f9bbb6c49
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-04-27 17:47:28 +0100

    Define User Schema

---
+++ server/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    minLength: [ 3, 'username must be at least 3 characters long' ],
+    unique: true,
+    required: [ true, 'username is required' ]
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [ true, 'email is required']
+  },
+  passwordHash: {
+    type: String,
+    required: [ true, 'Password is required' ]
+  },
+  picture: {
+    data: Buffer,
+    contentType: String
+  },
+  contacts : [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+})
+
+// Format the returned document 
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
