8022c499bbf254661d25029e157d3b22a85b3dcc
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-04-27 17:47:57 +0100

    Define Message Schema

---
+++ server/models/message.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const messageSchema = new mongoose.Schema({
+  content: {
+    type: mongoose.Schema.Types.Mixed,
+    minLength: [1, 'message content must be at least 1 character long'],
+    required: true
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  timestamp: {
+    type: Date,
+    required: true,
+  }
+})
+
+// Format the returned document 
+messageSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash
+  }
+})
+
+const Message = mongoose.model('Message', messageSchema)
+
+module.exports = Message
