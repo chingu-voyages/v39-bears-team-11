fefea688282a1481d948e3553d2db6c9fc3a602c
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-29 17:53:58 +0100

    :sparkles: Apply eslint suggestions

--- server/models/message.js
+++ server/models/message.js
@@ -4,23 +4,23 @@ const messageSchema = new mongoose.Schema({
   content: {
     type: mongoose.Schema.Types.Mixed,
     minLength: [1, 'message content must be at least 1 character long'],
-    required: true
+    required: true,
   },
   sender: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: 'User',
   },
   receiver: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: 'User',
   },
   timestamp: {
     type: Date,
     required: true,
-  }
+  },
 })
 
-// Format the returned document 
+// Format the returned document
 messageSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
@@ -28,7 +28,7 @@ messageSchema.set('toJSON', {
     delete returnedObject.__v
     // the passwordHash should not be revealed
     delete returnedObject.passwordHash
-  }
+  },
 })
 
 const Message = mongoose.model('Message', messageSchema)
