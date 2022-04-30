3f211d487ddb9b849bebcda7c34bd9522904b175
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-04-30 17:03:17 +0100

    :tada: Finish getMessageController logic

--- server/Controller/messageController.js
+++ server/Controller/messageController.js
@@ -1,11 +1,24 @@
 // import the Message model
 const Message = require('../models/message')
 
-const getMessageController = async (req, res) => {
+const getMessageController = async (req, res, next) => {
   // get the user id from the request body
   const { user_id: id } = req.body
 
-  Message.findOne({ user_id: id }, (err, result) => {
-      
+  // search database with received user_id
+  Message.findOne({ user_id: id }, (error, result) => {
+    // if there is error, pass it to the
+    // error handler middleware
+    if (error) {
+      next(error)
+    }
+
+    // if there is a match, set the statusCode
+    // and send the returned result
+    res.status(200).send(result)
   })
+}
+
+module.exports = {
+  getMessageController,
 }
