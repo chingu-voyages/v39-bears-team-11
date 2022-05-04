1397d8b2185587c4633895da0aa125dbe8fe01ad
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-05-04 12:56:54 +0100

    Test user icon

--- client/src/components/user/User.js
+++ client/src/components/user/User.js
@@ -1,9 +1,11 @@
 import styles from '../../styles/User.module.css'
+// import woman from '../../icons/profile-picture-woman-unsplash.jpg'
+import man from '../../icons/profile-picture-man-unsplash.jpg'
 
 function User() {
   return (
     <div className={styles.user}>
-      <img src="" alt="user" />
+      <img src={man} alt="user" />
     </div>
   )
 }
