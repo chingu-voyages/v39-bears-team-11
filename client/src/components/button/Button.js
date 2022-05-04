b744963c43d2e98ecab0e68517b0cd5648d629cf
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-05-04 13:00:34 +0100

    Update Button for generic use

--- client/src/components/button/Button.js
+++ client/src/components/button/Button.js
@@ -1,45 +1,14 @@
-import React, { Fragment } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { nanoid } from '@reduxjs/toolkit'
-import { login, logout } from '../../store/features/users/usersSlice'
 import styles from '../../styles/Button.module.css'
 
-function Button() {
-  const users = useSelector((state) => state.users)
-  const dispatch = useDispatch()
-
-  const handleLogin = () => {
-    dispatch(login({
-      id: nanoid(),
-      name: 'Moody Mudiaga',
-    }))
-  }
-
-  const handleLogout = () => {
-    dispatch(logout())
-  }
-
+function Button({ text, className, ...props }) {
   return (
-    <>
-      <h1>
-        Welcome,
-        {users.name}
-      </h1>
-      <button
-        type="button"
-        className={styles.Button}
-        onClick={handleLogin}
-      >
-        Log In
-      </button>
-      <button
-        type="button"
-        className={styles.Button}
-        onClick={handleLogout}
-      >
-        Log Out
-      </button>
-    </>
+    <button
+      type="button"
+      className={`${styles.button} ${className}`}
+      {...props}
+    >
+      {text}
+    </button>
   )
 }
 
