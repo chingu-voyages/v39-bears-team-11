159a8f6d5794d726d64f62727a42c09a418fde67
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-05-05 23:31:10 +0100

    Add search result components

---
+++ client/src/components/search_result/SearchResult.js
@@ -0,0 +1,68 @@
+import { useSelector } from 'react-redux'
+import { useRef } from 'react'
+import styles from '../../styles/SearchResults.module.css'
+import AddFriendButton from '../button/AddFriendButton'
+import UnfriendButton from '../button/UnfriendButton'
+import FriendTag from '../friend_tag/FriendTag'
+import Modal from '../modal/Modal'
+
+function SearchResult({
+  result,
+  container,
+  modalTitle,
+  modalText,
+  confirmButtonText,
+  formHandler,
+}) {
+  const friends = useSelector(({ user }) => user.friends)
+  const modalRef = useRef()
+
+  const showButton = function () {
+    if (container === 'people') {
+      const friend = friends.find((f) => f.id === result.id)
+
+      if (friend) {
+        return (
+          <FriendTag />
+        )
+      }
+      return (
+        <AddFriendButton handleAddFriendClick={() => modalRef.current.showModal()} />
+      )
+    }
+    return (
+      <UnfriendButton handleUnfriendClick={() => modalRef.current.showModal()} />
+    )
+  }
+
+  return (
+    <div
+      className={styles['search-result']}
+      data-id={result.id}
+    >
+      <div className={styles['search-result__description']}>
+        <div className={styles['search-result__image']}>
+          <img src={result.picture} alt={result.username} />
+        </div>
+        <div className={styles['search-result__text']}>
+          <p>{result.username}</p>
+        </div>
+      </div>
+      {
+        showButton()
+      }
+
+      <Modal
+        ref={modalRef}
+        title={modalTitle}
+        text={modalText}
+        onRequestClose={() => modalRef.current.close()}
+        confirmButtonText={confirmButtonText}
+        formHandler={formHandler}
+        formHandlerArgument={result.id}
+      />
+    </div>
+  )
+}
+
+export default SearchResult
---
+++ client/src/components/search_results/SearchResults.js
@@ -0,0 +1,25 @@
+import styles from '../../styles/SearchResults.module.css'
+import ContainerHeading from '../container_heading/ContainerHeading'
+import SearchResult from '../search_result/SearchResult'
+
+function SearchResults({ results, container, ...props }) {
+  return (
+    <>
+      { results.length > 0 && <ContainerHeading text="People" />}
+      <div className={styles['search-results']}>
+        {
+          results.map((result) => (
+            <SearchResult
+              key={result.id}
+              result={result}
+              container={container}
+              {...props}
+            />
+          ))
+        }
+      </div>
+    </>
+  )
+}
+
+export default SearchResults
