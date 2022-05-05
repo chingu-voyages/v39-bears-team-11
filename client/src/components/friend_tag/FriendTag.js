865d49d2a11cbb8ac5a55a6f685ab26abfaf9b57
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-05-05 23:27:41 +0100

    Add search result buttons and tags

---
+++ client/src/components/friend_tag/FriendTag.js
@@ -0,0 +1,13 @@
+import styles from '../../styles/FriendTag.module.css'
+
+function FriendTag() {
+  return (
+    <div
+      className={styles['friend-tag']}
+    >
+      Friend
+    </div>
+  )
+}
+
+export default FriendTag
