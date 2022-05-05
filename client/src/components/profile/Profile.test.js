9e07155a5ea6d2829b8bc4b27dd081885745492a
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-05-05 15:33:45 +0100

    :pencil_2: Fix typographical error

--- client/src/components/profile/Profile.test.js
+++ client/src/components/profile/Profile.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import Profile from './Profile'
 
 describe('Profile Page Feature', () => {
-  test('render the Profile component', () => {
+  test('render the empty Profile component', () => {
     render(<Profile />)
     expect(screen.getByTestId('empty')).toBeEmptyDOMElement()
   })
