0f3a3227a8b0f044a924b9c9b52dbce5b74e0251
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-05-05 15:48:19 +0100

    :recycle: Refactor Profile.test.js

--- client/src/components/profile/Profile.test.js
+++ client/src/components/profile/Profile.test.js
@@ -3,13 +3,13 @@ import { render, screen } from '@testing-library/react'
 import Profile from './Profile'
 
 describe('Profile Page Feature', () => {
+  beforeEach(() => render(<Profile />))
+
   test('render the empty Profile component', () => {
-    render(<Profile />)
     expect(screen.getByTestId('empty')).toBeEmptyDOMElement()
   })
 
   test('render the navbar in profile component', () => {
-    render(<Profile />)
     expect(screen.getByTestId('empty')).toContainHTML(
       '<div className={styles.navbar}></div>',
     )
