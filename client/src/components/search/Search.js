0658472ee2e2280ee029baec1c1bd944bbb30437
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-05-05 23:19:05 +0100

    Add test request to Search Import MainPage component and pass main search page as child Add search handler and friend handler

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,12 +1,20 @@
+import axios from 'axios'
 import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import SearchBox from '../searchbox/SearchBox'
 import SearchResults from '../search_results/SearchResults'
 import MainPage from '../main_page/MainPage'
+import { addFriend } from '../../store/features/users/usersSlice'
 // import peopleService from '../../services/people'
 
-const axios = require('axios')
-
 function Search() {
+  const userState = useSelector(({ user }) => user)
+  const {
+    id, username, email, token, picture, friends,
+  } = userState
+  console.log(friends)
+  const dispatch = useDispatch()
+
   const container = 'people'
   const [currentResults, setCurrentResults] = useState([])
 
@@ -14,10 +22,24 @@ function Search() {
     event.preventDefault()
     const searchKeywords = event.target.search.value
     console.log(searchKeywords)
+
+    /* Test search with json server */
     const response = await axios.get('http://localhost:5000/users')
     setCurrentResults(response.data.slice(0, 10))
-    console.log(currentResults)
-    // const results = await peopleService.search(searchKeywords)
+
+    /* Send request to backend using peopleService */
+    // results = await peopleService.search(searchKeywords)
+    // setCurrentResults(results)
+  }
+
+  const handleAdd = (personId) => {
+    /* Dispatch a addFriend action to user store */
+    dispatch(addFriend({
+      username,
+      email,
+      picture,
+      friends: friends.map((friend) => friend.id).concat(personId),
+    }, id, token))
   }
 
   return (
@@ -30,6 +52,10 @@ function Search() {
         <SearchResults
           results={currentResults}
           container={container}
+          modalTitle="Confirm Friend Request"
+          modalText="Are you sure you want to add this person to your friends list ?"
+          confirmButtonText="Add"
+          formHandler={handleAdd}
         />
       </div>
     </MainPage>
