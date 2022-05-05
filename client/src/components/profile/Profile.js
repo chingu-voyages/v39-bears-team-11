745cb175ce71dbd8871196444ab7ff69a2cea0b2
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-05-05 16:07:58 +0100

    :white_check_mark: Pass render navbar in profile component test

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,7 +1,12 @@
 import { Fragment } from 'react'
+import Navbar from '../navbar/Navbar'
 
 function Profile() {
-  return <div data-testid="empty"></div>
+  return (
+    <div data-testid="empty">
+      <Navbar />
+    </div>
+  )
 }
 
 export default Profile
