4bfbd29f93ca96ae6c7071448afffe65d5372069
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-05-06 20:24:59 +0100

    :fire: Remove instances of reportWebVitals

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,10 @@
 /* eslint-disable semi */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { Provider } from 'react-redux'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
 import { store } from './store/store'
+import App from './App'
+import './index.css'
 
 // eslint-disable-next-line no-undef
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -19,8 +18,3 @@ root.render(
     </React.StrictMode>
   </Provider>,
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
