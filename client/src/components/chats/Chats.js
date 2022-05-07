4dedf6e87b8d7fab145ae2198ad105862fbd2fd2
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-05-07 23:00:14 +0100

    Refactored Chats component to match newwly pulled and merged changes

--- client/src/components/chats/Chats.js
+++ client/src/components/chats/Chats.js
@@ -1,16 +1,13 @@
 import StartAChatButton from '../button/StartAChatButton'
-import MainPage from '../main_page/MainPage'
 
 function Chats() {
   return (
-    <MainPage>
-      <div
-        id="chats"
-        className="container main"
-      >
-        <StartAChatButton />
-      </div>
-    </MainPage>
+    <div
+      id="chats"
+      className="container main"
+    >
+      <StartAChatButton />
+    </div>
   )
 }
 
