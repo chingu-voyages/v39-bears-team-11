e109a0c7ee508da752679e52d810b487e1fdd1e8
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-05-07 19:59:48 +0100

    :white_check_mark: Pass should render profile icons test

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,10 +1,16 @@
 /* eslint-disable react/self-closing-comp */
+import editIcon from '../../icons/profile/profile-edit-icon.svg'
+import deleteIcon from '../../icons/profile/profile-delete-icon.svg'
 
 function Profile() {
   return (
     <>
       <header>
         <h2>Profile</h2>
+        <div>
+          <img src={editIcon} alt="edit icon" />
+          <img src={deleteIcon} alt="delete icon" />
+        </div>
       </header>
       <main>
         <p>Component</p>
