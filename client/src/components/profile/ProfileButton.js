03a9fc70cf699c6e20bfb66bcc9c4d70cb5fbc5c
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-05-10 17:33:34 +0100

    :recycle: Refactor profile button component

--- client/src/components/profile/ProfileButton.js
+++ client/src/components/profile/ProfileButton.js
@@ -1,8 +1,13 @@
+/* eslint-disable object-curly-newline */
 import styles from '../../styles/Profile-styles/ProfileButton.module.css'
 
-function ProfileButton({ onClick, imgSrc, altText }) {
+function ProfileButton({ onClick, imgSrc, altText, styling }) {
   return (
-    <button type="button" className={styles.Profile__icons} onClick={onClick}>
+    <button
+      type="button"
+      className={`${styles.Profile__icons}, ${styling || ''}`}
+      onClick={onClick}
+    >
       <img src={imgSrc} alt={altText} />
     </button>
   )
