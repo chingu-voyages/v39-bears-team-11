8667409e19cac0d43508a233511ae77e9c3f903b
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-05-10 17:31:25 +0100

    :recycle: Refactor modal component

--- client/src/components/modal/Modal.js
+++ client/src/components/modal/Modal.js
@@ -3,34 +3,29 @@ import ConfirmButton from '../button/ConfirmButton'
 import CancelButton from '../button/CancelButton'
 import styles from '../../styles/Modal.module.css'
 
-const Modal = forwardRef(({
-  title,
-  text,
-  onRequestClose,
-  confirmButtonText,
-  buttonClasses,
-  formHandler,
-  formHandlerArgument,
-}, ref) => (
-  <dialog
-    ref={ref}
-    className={styles.modal}
+const Modal = forwardRef(
+  (
+    {
+      title,
+      text,
+      onRequestClose,
+      confirmButtonText,
+      buttonClasses,
+      formHandler,
+      formHandlerArgument,
+    },
+    ref,
+  ) => (
+    <dialog ref={ref} className={styles.modal}>
+      <h4>{title}</h4>
+      <p>{text}</p>
+      <form method="dialog" onSubmit={() => formHandler(formHandlerArgument)}>
+        <ConfirmButton text={confirmButtonText} buttonClasses={buttonClasses} />
+      </form>
 
-  >
-    <h4>{title}</h4>
-    <p>{text}</p>
-    <form
-      method="dialog"
-      onSubmit={() => formHandler(formHandlerArgument)}
-    >
-      <ConfirmButton
-        text={confirmButtonText}
-        buttonClasses={buttonClasses}
-      />
-    </form>
-
-    <CancelButton clickHandler={onRequestClose} />
-  </dialog>
-))
+      <CancelButton clickHandler={onRequestClose} />
+    </dialog>
+  ),
+)
 
 export default Modal
