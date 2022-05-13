0c366e862e3731aa0fea62f70bb9f24d9ba1c4b6
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-05-13 18:35:51 +0100

    Add Messages component Add messages state Add auto scroll to Message

--- client/src/components/messages/Messages.js
+++ client/src/components/messages/Messages.js
@@ -1,20 +1,34 @@
+import { useRef, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { nanoid } from '@reduxjs/toolkit'
+import styles from '../../styles/Messages.module.css'
 import Message from '../message/Message'
 
 function Messages() {
-  const chats = useSelector(({ chats }) => chats)
+  const messagesState = useSelector(({ messages }) => messages)
   const friends = useSelector(({ user }) => user.friends)
-  const { messages, currentFriendId } = chats
-  const currentFriend = friends.find((f) => f.id === currentFriendId)
+  const { chats, currentFriendId } = messagesState
+
+  const currentFriendChat = chats.find((chat) => chat.friendId === currentFriendId)
+  const currentFriendMessages = currentFriendChat.messages
+  const currentFriendInfo = friends.find((f) => f.id === currentFriendId)
+
+  const scrollRef = useRef()
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView(false, { behavior: 'smooth' })
+  }, [currentFriendMessages])
 
   return (
-    <div className={styles.messges}>
-      { messages.map((message) => (
+    <div className={styles.messages}>
+      { currentFriendMessages.map((message) => (
         <Message
+          ref={scrollRef}
+          key={nanoid()}
           senderId={message.sender}
           content={message.content}
           time={message.timestamp}
-          currentFriend={currentFriend}
+          currentFriend={currentFriendInfo}
         />
       ))}
     </div>
