af200d737411c58aa378fa811a4321212a59e02e
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-05-13 23:23:05 +0100

    create sortArrayOfObjects helper function

--- client/src/components/chats/Chats.js
+++ client/src/components/chats/Chats.js
@@ -3,9 +3,10 @@ import { useSelector } from 'react-redux'
 import StartAChatButton from '../button/StartAChatButton'
 import ModalStartAChat from '../modal/ModalStartAChat'
 import LatestChats from '../latest_chats/LatestChats'
+import { sortArrayOfObjects } from '../../utils/helper'
 
 // mocked messages data
-const retrievedMessages = [
+const chatsMockData = [
   {
     id: '3',
     messages: [
@@ -21,17 +22,18 @@ const retrievedMessages = [
     id: '6',
     messages: [
       {
+        content: 'Show. Alright I will look over chat array, sort, and return latest message.',
+        sender_id: '61cdd39a5a14f24e4f2f89c7',
+        receiver_id: '6',
+        timestamp: 1652362533,
+      },
+      {
         content: 'Look over chat array, sort, return latest message.',
         sender_id: '',
         receiver_id: '6',
         timestamp: 1652362500,
       },
-      {
-        content: 'Alright I will look over chat array, sort, and return latest message.',
-        sender_id: '61cdd39a5a14f24e4f2f89c7',
-        receiver_id: '6',
-        timestamp: 1652362533,
-      },
+
     ],
   },
   {
@@ -44,16 +46,16 @@ const retrievedMessages = [
         timestamp: 1652261968,
       },
       {
+        content: 'Show me and you always say tomorrow and it is never done.',
+        sender_id: '33',
+        receiver_id: '61cdd39a5a14f24e4f2f89c73',
+        timestamp: 1652271970,
+      },
+      {
         content: 'Ok thank you for all the details I should implement that tomorrow.',
         sender_id: '61cdd39a5a14f24e4f2f89c7',
         receiver_id: '33',
         timestamp: 1652271969,
-      },
-      {
-        content: 'You always say tomorrow and it is never done.',
-        sender_id: '33',
-        receiver_id: '61cdd39a5a14f24e4f2f89c73',
-        timestamp: 1652271970,
       },
 
     ],
@@ -85,19 +87,37 @@ function Chats() {
     setShow(false)
   }
 
-  const lastMessagesData = retrievedMessages.reduce(
-    (newArr, currentMessage) => {
-      const friendId = currentMessage.id
+  const lastestChats = chatsMockData.reduce(
+    (newArr, currentChat) => {
+      //  Use provided friend id (from user messages) to get the friend index in the friends data.
+      const friendId = currentChat.id
       const friendIndex = friends.findIndex((friend) => friend.id.toString() === friendId)
-      const friendMessages = currentMessage.messages
+      //  Use friend index (from user friends array) to get friend's picture and username
+      const Picture = friends[friendIndex].picture
+      const Username = friends[friendIndex].username
 
+      //  Use all the messages from the current friend.
+      const allMessages = currentChat.messages
+      //  Use helper function to sort these messages and return only the latest message
+      //  sortArrayOfObjects arguments:
+      //     -array of objects
+      //     -object's property which the sorting will be based on
+      //     -choice of either ascending or descending order
+      //     -choice of returning either first item, last item, or the whole array
+      const latestMessage = sortArrayOfObjects(allMessages, 'timestamp', 'desc', 'firstItem')
+      // Use the latest message to get the timestamp and the message content
+      const Content = latestMessage.content
+      const Timestamp = latestMessage.timestamp
+
+      // Define new object with all prepared data
       const newInstance = {
         id: friendId,
-        picture: friends[friendIndex].picture,
-        username: friends[friendIndex].username,
-        message: friendMessages[friendMessages.length - 1].content,
-        time: friendMessages[friendMessages.length - 1].timestamp,
+        picture: Picture,
+        username: Username,
+        message: Content,
+        time: Timestamp,
       }
+      // Add this object to the list that is being created in this reduce function
       newArr.push(newInstance)
       return newArr
     },
@@ -111,7 +131,7 @@ function Chats() {
     >
       <StartAChatButton onOpenModal={onOpenModal} />
       <ModalStartAChat friends={friends} show={show} onCloseModal={onCloseModal} />
-      <LatestChats chats={lastMessagesData} />
+      <LatestChats chats={lastestChats} />
     </div>
   )
 }
--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,1 +1,30 @@
 // where an helper function lives
+
+// Sorting function that returns a sorted array or the first item of it
+//
+// arguments:
+//   arr - the array that will be sorted
+//   property - the property of the object by which the array will be sorted
+//   order =  'asc' - sort in ascending order (default)
+//            'desc' - sort in desceceding order
+//   receive = 'array' - funtion will return the sorted array (default)
+//             'firstItem' - function will return the first item of the sorted array
+//             'lastItem'  - function will return the last item of the sorted array
+const sortArrayOfObjects = (
+  arr,
+  property,
+  order = 'asc',
+  receive = 'array',
+) => {
+  const sortedArr = arr.sort((a, b) => {
+    if (order === 'asc') return a[property] - b[property]
+    if (order === 'desc') return b[property] - a[property]
+    return 'error: wrong _order_ argument'
+  })
+  if (receive === 'array') return sortedArr
+  if (receive === 'firstItem') return sortedArr[0]
+  if (receive === 'lastItem') return sortedArr[sortedArr.length - 1]
+  return 'error: wrong _receive_ argument'
+}
+
+export { sortArrayOfObjects }
