17a42f933b8cf7ed44a7124e3fa6dffaebd6e44f
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-05-13 18:43:45 +0100

    Add Message component
    Add user info and dynamic message css classes

--- client/src/components/message/Message.js
+++ client/src/components/message/Message.js
@@ -1,36 +1,39 @@
+import { forwardRef } from 'react'
 import { useSelector } from 'react-redux'
 import styles from '../../styles/Messages.module.css'
+import MessageHead from '../message_head/MessageHead'
+import MessageBody from '../message_body/MessageBody'
 
-function Message({
+const Message = forwardRef(({
   senderId,
   content,
   time,
   currentFriend,
-}) {
+}, ref) => {
   const userState = useSelector(({ user }) => user)
   const { username, picture } = userState
 
-  const isFriendMessage = () => senderId === currentFriend.id
-  const getMessageClass = isFriendMessage() ? 'message--friend' : 'message--user'
-  const getMessageHeadClass = isFriendMessage() ? 'message__head--friend' : 'message__head--user'
-  const getMessageBodyClass = isFriendMessage() ? 'message__body--friend' : 'message__body--user'
-  const getMessageHeadPicture = isFriendMessage() ? currentFriend.picture : picture
-  const getMessageHeadName = isFriendMessage() ? currentFriend.username : username
+  const isFriendMessage = senderId === currentFriend.id
+  const messageClass = isFriendMessage ? 'message--friend' : 'message--user'
+  const messageHeadClass = isFriendMessage ? 'message__head--friend' : 'message__head--user'
+  const messageBodyClass = isFriendMessage ? 'message__body--friend' : 'message__body--user'
+  const messageHeadPicture = isFriendMessage ? currentFriend.picture : picture
+  const messageHeadName = isFriendMessage ? currentFriend.username : username
 
   return (
-    <div className={`${styles.message} ${styles[getMessageClass]}`}>
+    <div className={`${styles.message} ${styles[messageClass]}`} ref={ref}>
       <MessageHead
-        classes={getMessageHeadClass}
-        picture={getMessageHeadPicture}
-        name={getMessageHeadName}
+        classes={messageHeadClass}
+        picture={messageHeadPicture}
+        name={messageHeadName}
         time={time}
       />
       <MessageBody
-        classes={getMessageBodyClass}
+        classes={messageBodyClass}
         content={content}
       />
     </div>
   )
-}
+})
 
 export default Message
