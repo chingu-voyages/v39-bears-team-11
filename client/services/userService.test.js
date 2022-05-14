fd07f947eeb2c7a3a193413e483c54a26f7ad272
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-05-14 21:01:25 +0100

    :test_tube: Add userService unit tests

--- client/services/userService.test.js
+++ client/services/userService.test.js
@@ -5,26 +5,74 @@ const userService = require('./userService')
 
 jest.mock('./userService.js')
 
-const user = 'adalovelace'
 const id = 1
+const username = 'adalovelace'
+const email = 'adalove@gmail.com'
 const token = 'thisisamocktoken'
+const imgData = 'https://fakeimgdata.com'
 const response = { data: ['user1', 'user2', 'user3'] }
 
 describe('User Service', () => {
   afterEach(() => jest.clearAllMocks())
+  let addFriend = jest.spyOn(userService, 'addFriend')
   it('should call addFriend function', () => {
-    const addFriend = jest.spyOn(userService, 'addFriend')
     addFriend()
     expect(addFriend).toHaveBeenCalled()
+    addFriend.mockClear()
 
-    addFriend(user, id, token)
-    expect(addFriend).toHaveBeenCalledWith(user, id, token)
-  })
+    addFriend()
+    expect(addFriend).toHaveBeenCalledTimes(1)
 
-  it('should implement addFriend function', () => {
-    const addFriend = userService.addFriend.mockImplementation(() =>
+    addFriend(id, username, token)
+    expect(addFriend).toHaveBeenCalledWith(id, username, token)
+
+    addFriend = userService.addFriend.mockImplementation(() =>
       Promise.resolve(response.data),
     )
-    expect(addFriend()).resolves.toEqual(response.data)
+    expect(addFriend()).resolves.toEqual(['user1', 'user2', 'user3'])
+  })
+
+  it('should return response data', async () => {
+    const unFriend = jest.spyOn(userService, 'unFriend')
+
+    unFriend(id, username, token)
+    expect(unFriend).toHaveBeenCalledWith(id, username, token)
+
+    const req = unFriend.mockResolvedValueOnce(response.data)
+    const res = await req()
+    expect(res).toStrictEqual(['user1', 'user2', 'user3'])
+  })
+
+  it('should return response data', async () => {
+    const updateProfile = jest.spyOn(userService, 'updateProfile')
+
+    updateProfile(id, username, email, token)
+    expect(updateProfile).toHaveBeenCalledWith(id, username, email, token)
+
+    const req = updateProfile.mockResolvedValueOnce(response.data)
+    const res = await req()
+    expect(res).toStrictEqual(['user1', 'user2', 'user3'])
+  })
+
+  it('should return response data', async () => {
+    const deleteProfile = jest.spyOn(userService, 'deleteProfile')
+
+    deleteProfile(id, token)
+    expect(deleteProfile).toHaveBeenCalledWith(id, token)
+
+    const req = deleteProfile.mockResolvedValueOnce(response.data)
+    const res = await req()
+    expect(res).toEqual(['user1', 'user2', 'user3'])
+  })
+
+  it('should return response data', async () => {
+    const uploadPhoto = jest.spyOn(userService, 'uploadPhoto')
+
+    uploadPhoto(id, imgData, token)
+    expect(uploadPhoto).toHaveBeenCalledWith(id, imgData, token)
+
+    const req = uploadPhoto.mockResolvedValueOnce(response.data)
+    const res = await req()
+    expect(res).toEqual(['user1', 'user2', 'user3'])
   })
 })
