18d85dacc05bf5ae80bc3d49ec99efb2009d2c51
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-05-17 20:03:41 +0100

    :recycle: Refactor userService test suite

--- client/services/userService.test.js
+++ client/services/userService.test.js
@@ -19,6 +19,9 @@ describe('User Service', () => {
   afterAll(() => jest.clearAllMocks())
   const addFriend = jest.spyOn(userService, 'addFriend')
   const unFriend = jest.spyOn(userService, 'unFriend')
+  const updateProfile = jest.spyOn(userService, 'updateProfile')
+  const deleteProfile = jest.spyOn(userService, 'deleteProfile')
+  const uploadPhoto = jest.spyOn(userService, 'uploadPhoto')
 
   it('should call addFriend function', () => {
     addFriend()
@@ -40,43 +43,53 @@ describe('User Service', () => {
     )
   })
 
-  it('should return response data', async () => {
+  it('should call unFriend and with correct params', () => {
     unFriend(id, username, token)
+    expect(unFriend).toHaveBeenCalled()
+    expect(unFriend).toHaveBeenCalledTimes(1)
     expect(unFriend).toHaveBeenCalledWith(id, username, token)
+  })
 
+  it('should call unFriend and return response data', async () => {
     const req = unFriend.mockResolvedValueOnce(response.data)
     const res = await req()
     expect(res).toEqual(expect.arrayContaining([expect.any(String)]))
   })
 
-  it('should return response data', async () => {
-    const updateProfile = jest.spyOn(userService, 'updateProfile')
-
+  it('should call updateProfile and with correct params', () => {
     updateProfile(id, username, email, token)
+    expect(updateProfile).toBeCalled()
+    expect(updateProfile).toBeCalledTimes(1)
     expect(updateProfile).toHaveBeenCalledWith(id, username, email, token)
+  })
 
+  it('should call updateProfile and return response data', async () => {
     const req = updateProfile.mockResolvedValueOnce(response.data)
     const res = await req()
     expect(res).toStrictEqual(expect.arrayContaining([expect.any(String)]))
   })
 
-  it('should return response data', async () => {
-    const deleteProfile = jest.spyOn(userService, 'deleteProfile')
-
+  it('should call deleteProfile and with correct params', () => {
     deleteProfile(id, token)
+    expect(deleteProfile).toBeCalled()
+    expect(deleteProfile).toBeCalledTimes(1)
     expect(deleteProfile).toHaveBeenCalledWith(id, token)
+  })
 
+  it('should call deleteProfile and return response data', async () => {
     const req = deleteProfile.mockResolvedValueOnce(response.data)
     const res = await req()
     expect(res).toEqual(expect.arrayContaining([expect.any(String)]))
   })
 
-  it('should return response data', async () => {
-    const uploadPhoto = jest.spyOn(userService, 'uploadPhoto')
-
+  it('should call uploadPhoto and with correct params', () => {
     uploadPhoto(id, imgData, token)
+    expect(uploadPhoto).toHaveBeenCalled()
+    expect(uploadPhoto).toHaveBeenCalledTimes(1)
     expect(uploadPhoto).toHaveBeenCalledWith(id, imgData, token)
+  })
 
+  it('should return response data', async () => {
     const req = uploadPhoto.mockResolvedValueOnce(response.data)
     const res = await req()
     expect(res).toEqual(expect.arrayContaining([expect.any(String)]))
