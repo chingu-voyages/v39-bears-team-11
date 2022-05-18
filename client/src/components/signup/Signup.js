248940782be2243a84ed162c07241b0497af0432
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-05-18 23:13:55 +0100

    create basic structure and styling

--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -1,6 +1,18 @@
+import styles from '../../styles/Signup.module.css'
+import Logo from '../logo/Logo'
+import Illustration from '../../icons/signup/signup-illustration.png'
+
 function Signup() {
   return (
-    <div>You are at the Signup page</div>
+    <div id="signup" className={styles.Signup}>
+      <div className={styles['Signup__logo-illustration-container']}>
+        <Logo className={styles.Signup__logo} />
+        <img src={Illustration} alt="Chats comming out of the computer screen. Conversation between 2 people." className={styles.Signup__illustration} />
+      </div>
+      <div className="forms">
+        Right
+      </div>
+    </div>
   )
 }
 
