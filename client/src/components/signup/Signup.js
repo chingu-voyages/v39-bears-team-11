17b31a0b0931732b90432da45a77c94c933ca11c
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-05-19 20:59:55 +0100

    trip signup-illustration image

--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -10,19 +10,23 @@ function Signup() {
   return (
     <div id="signup" className={styles.Signup}>
       <div className={styles['Signup__logo-illustration-container']}>
-        <Logo className={styles['Signup__logo-blue']} />
-        <Logo isWhite className={styles['Signup__logo-white']} />
+        <Logo />
         <img src={Illustration} alt="Chats comming out of the computer screen. Conversation between 2 people." className={styles.Signup__illustration} />
       </div>
-      <div className={styles.Signup__form__container}>
+      <div className={styles.Signup__main}>
+        <div className={styles['Signup__main__logo-container']}>
+          <Logo isWhite />
+        </div>
         <div className={styles['Signup__form__control-buttons']}>
           <ChoiceButton type="login" isActive={formChoice === 'login'} handleFormChoiceClick={handleFormChoiceClick} />
           <ChoiceButton type="signup" isActive={formChoice === 'signup'} handleFormChoiceClick={handleFormChoiceClick} />
         </div>
-        <h4 className={styles.Signup__form__heading}>
-          {formChoice === 'login' && 'Login To Your Account'}
-          {formChoice === 'signup' && 'Create your Account'}
-        </h4>
+        <div className={styles.Signup__form__container}>
+          <h4 className={styles.Signup__form__heading}>
+            {formChoice === 'login' && 'Login To Your Account'}
+            {formChoice === 'signup' && 'Create your Account'}
+          </h4>
+        </div>
       </div>
 
     </div>
