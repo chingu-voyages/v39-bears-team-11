36234bca2f91d8ccfb5908465311163e7ac1ea18
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-05-19 23:40:17 +0100

    add multiple responsive features

--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -26,6 +26,12 @@ function Signup() {
             {formChoice === 'login' && 'Login To Your Account'}
             {formChoice === 'signup' && 'Create your Account'}
           </h4>
+          <form className={styles.Signup__form}>
+            <input type="text" className={styles.Signup__form__input} />
+            <input type="text" className={styles.Signup__form__input} />
+            <input type="text" className={styles.Signup__form__input} />
+            <button type="submit" className={styles.Signup__form__button}>sd</button>
+          </form>
         </div>
       </div>
 
