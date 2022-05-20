11cee8319c43583a644bbbe7ce77bc2b26f5d672
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-05-20 11:16:55 +0100

    add type prop to Button component

--- client/src/components/signup/SignupForm.js
+++ client/src/components/signup/SignupForm.js
@@ -1,17 +1,18 @@
+import Button from '../button/Button'
 import styles from '../../styles/SignupForm.module.css'
 
 function SignupForm({ formChoice }) {
   return (
-    <div className={styles.Signup__form__container}>
-      <h4 className={styles.Signup__form__heading}>
+    <div className={styles.SignupForm__container}>
+      <h4 className={styles.SignupForm__heading}>
         {formChoice === 'login' && 'Login To Your Account'}
         {formChoice === 'signup' && 'Create Your Account'}
       </h4>
-      <form className={styles.Signup__form}>
-        <input type="text" className={styles.Signup__form__input} />
-        <input type="text" className={styles.Signup__form__input} />
-        <input type="text" className={styles.Signup__form__input} />
-        <button type="submit" className={styles.Signup__form__button}>ssd</button>
+      <form className={styles.SignupForm}>
+        <input type="text" aria-label="Your Name" className={styles.SignupForm__input} />
+        <input type="text" aria-label="Your Email" className={styles.SignupForm__input} />
+        <input type="text" aria-label="Your Password" className={styles.SignupForm__input} />
+        <button type="submit" className={styles.SignupForm__button}>ssd</button>
       </form>
     </div>
   )
