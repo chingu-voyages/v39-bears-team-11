4fd2f73c14acb75b9563e5cd4f28a113b348cb11
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-05-20 17:05:24 +0100

    add icons to the inputs

--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -24,7 +24,6 @@ function Signup() {
         </div>
         <SignupForm formChoice={formChoice} />
       </div>
-
     </div>
   )
 }
--- client/src/components/signup/SignupForm.js
+++ client/src/components/signup/SignupForm.js
@@ -1,5 +1,8 @@
 import SignupFormButton from '../button/SignupFormButton'
 import styles from '../../styles/SignupForm.module.css'
+import userIcon from '../../icons/signup/signup-user-icon.png'
+import emailIcon from '../../icons/signup/signup-email-icon.png'
+import passwordIcon from '../../icons/signup/signup-password-icon.png'
 
 function SignupForm({ formChoice }) {
   return (
@@ -9,9 +12,36 @@ function SignupForm({ formChoice }) {
         {formChoice === 'signup' && 'Create Your Account'}
       </h4>
       <form className={styles.SignupForm}>
-        <input type="text" aria-label="Your Name" className={styles.SignupForm__input} />
-        <input type="text" aria-label="Your Email" className={styles.SignupForm__input} />
-        <input type="text" aria-label="Your Password" className={styles.SignupForm__input} />
+        <div className={styles['SignupForm__input-container']}>
+          <input
+            type="text"
+            aria-label="Your Name"
+            placeholder="Your Name"
+            className={styles.SignupForm__input}
+          />
+          <img src={userIcon} alt="user" className={styles.SignupForm__icon} />
+        </div>
+        {(formChoice === 'signup')
+          ? (
+            <div className={styles['SignupForm__input-container']}>
+              <input
+                type="text"
+                aria-label="Your Email"
+                placeholder="Your Email"
+                className={styles.SignupForm__input}
+              />
+              <img src={emailIcon} alt="email" className={styles.SignupForm__icon} />
+            </div>
+          ) : null}
+        <div className={styles['SignupForm__input-container']}>
+          <input
+            type="text"
+            aria-label="Your Password"
+            placeholder="Password"
+            className={styles.SignupForm__input}
+          />
+          <img src={passwordIcon} alt="password" className={styles.SignupForm__icon} />
+        </div>
         <SignupFormButton choice={formChoice} type="submit" className={styles.SignupForm__button} />
       </form>
     </div>
