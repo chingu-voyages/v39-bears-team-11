4d809b1c983cad76a86db4993a093bafdb3a8982
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-05-20 20:36:01 +0100

    create credentials state variable

--- client/src/components/signup/SignupForm.js
+++ client/src/components/signup/SignupForm.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import SignupFormButton from '../button/SignupFormButton'
 import styles from '../../styles/SignupForm.module.css'
 import userIcon from '../../icons/signup/signup-user-icon.png'
@@ -5,6 +6,22 @@ import emailIcon from '../../icons/signup/signup-email-icon.png'
 import passwordIcon from '../../icons/signup/signup-password-icon.png'
 
 function SignupForm({ formChoice, handleSubmit }) {
+  const [credentials, SetCredentials] = useState({
+    name: '',
+    email: '',
+    password: '',
+    formErrors: { name: '', email: '', password: '' },
+    nameValid: false,
+    emailValid: false,
+    passwordValid: false,
+    formValid: false,
+  })
+  const handleInput = (e) => {
+    const property = e.target.name
+    const { value } = e.target
+    SetCredentials({ ...credentials, [property]: value })
+    console.log(credentials)
+  }
   return (
     <div className={styles.SignupForm__container}>
       <h4 className={styles.SignupForm__heading}>
@@ -17,6 +34,9 @@ function SignupForm({ formChoice, handleSubmit }) {
             type="text"
             aria-label="Your Name"
             placeholder="Your Name"
+            name="name"
+            value={credentials.name}
+            onChange={(event) => handleInput(event)}
             className={styles.SignupForm__input}
             required
           />
@@ -29,6 +49,9 @@ function SignupForm({ formChoice, handleSubmit }) {
                 type="text"
                 aria-label="Your Email"
                 placeholder="Your Email"
+                name="email"
+                value={credentials.email}
+                onChange={(event) => handleInput(event)}
                 className={styles.SignupForm__input}
                 required
               />
@@ -40,6 +63,9 @@ function SignupForm({ formChoice, handleSubmit }) {
             type="text"
             aria-label="Your Password"
             placeholder="Password"
+            name="password"
+            value={credentials.password}
+            onChange={(event) => handleInput(event)}
             className={styles.SignupForm__input}
             required
           />
