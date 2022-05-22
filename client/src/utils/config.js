d80257b897542deae808df225d0695913132904d
Mudiaga Moody Uwojeya <mudiagauwojeya@gmail.com>
2022-05-22 22:18:23 +0100

    :new: Add axiosConfigObject function

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,1 +1,24 @@
 // where api config or similar files may live
+
+/**
+ * @name axiosConfigObject
+ * @summary A reusable function to generate the axios config object to pass
+ * as the third parameter to axios requests.
+ * @param {string} token
+ * @returns A configuration object to pass to the axios API requests.
+ * @example
+ * const token = '8jdnoake100343jdnsokelselsue1soq'
+ * const config = axiosConfigObject(token)
+ * console.log(config)
+ * {
+ * headers: {Authorization: '8jdnoake100343jdnsokelselsue1soq' }
+ * }
+ */
+const axiosConfigObject = (token) => {
+  const config = {
+    headers: { Authorization: `${token}` },
+  }
+  return config
+}
+
+export default axiosConfigObject
