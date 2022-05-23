852562a64b75dd93917556f3d8d097a3a4343bd8
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-05-23 15:16:00 +0100

    refactor form validation to validate only on submit

--- client/src/components/button/SignupFormButton.js
+++ client/src/components/button/SignupFormButton.js
@@ -1,7 +1,9 @@
 import Button from './Button'
 import styles from '../../styles/Button.module.css'
 
-function SignupFormButton({ type, choice, className }) {
+function SignupFormButton({
+  type, choice, className,
+}) {
   return (
     <Button
       text={choice}
--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Logo from '../logo/Logo'
 import Illustration from '../../icons/signup/signup-illustration.png'
 import ChoiceButton from '../button/ChoiceButton'
@@ -8,10 +8,21 @@ import styles from '../../styles/Signup.module.css'
 function Signup() {
   const [formChoice, setFormChoice] = useState('signup')
   const handleFormChoiceClick = (choice) => (setFormChoice(choice))
-  const handleSubmit = (event, credentialsData) => {
-    event.preventDefault()
-    console.log('Here we have an object with name, email, password', credentialsData)
-  }
+  const [credentials, setCredentials] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
+
+  useEffect(() => {
+    // Run everytime when validated [credentials] have been updated.
+    //
+    // if statement to prevent useEffect running on first render with empty data
+    if (credentials.name && credentials.password) {
+      console.log('Here are validated credentials: ', credentials)
+    }
+  }, [credentials])
+
   return (
     <div id="signup" className={styles.Signup}>
       <div className={styles.Signup__side}>
@@ -26,7 +37,7 @@ function Signup() {
           <ChoiceButton choice="login" isActive={formChoice === 'login'} handleFormChoiceClick={handleFormChoiceClick} />
           <ChoiceButton choice="signup" isActive={formChoice === 'signup'} handleFormChoiceClick={handleFormChoiceClick} />
         </div>
-        <SignupForm formChoice={formChoice} handleSubmit={handleSubmit} />
+        <SignupForm formChoice={formChoice} setCredentials={setCredentials} />
       </div>
     </div>
   )
