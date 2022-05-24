2d3e5038181994ab75c692427b366e279bdd7b38
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-05-24 16:30:44 +0100

    Refactor user camera modal and add comments

--- client/src/components/modal/UseCameraModal.js
+++ client/src/components/modal/UseCameraModal.js
@@ -10,7 +10,6 @@ const UseCameraModal = forwardRef(({
   confirmButtonText,
 }, ref) => {
   const [selectedFile, setSelectedFile] = useState(null)
-  console.log(selectedFile)
   const [stream, setStream] = useState(null)
   const [video, setVideo] = useState(null)
   const [canvas, setCanvas] = useState(null)
@@ -22,6 +21,19 @@ const UseCameraModal = forwardRef(({
     setCanvas(ref.current.children[2])
   }, [])
 
+  /* When use camera modal is open, reate a stream  */
+  /* and assign it to the src of our video element */
+  useEffect(() => {
+    (async () => {
+      if (isOpen) {
+        /* Requesting User Permission */
+        const currentStream = await navigator.mediaDevices.getUserMedia({ video: true })
+        setStream(currentStream)
+        video.srcObject = currentStream
+      }
+    })()
+  }, [isOpen])
+
   const pauseStream = () => {
     // eslint-disable-next-line no-param-reassign
     stream.getTracks().forEach((track) => { track.enabled = false })
@@ -32,64 +44,35 @@ const UseCameraModal = forwardRef(({
     stream.getTracks().forEach((track) => { track.enabled = true })
   }
 
+  const showCanvas = () => {
+    video.classList.add('hidden')
+    canvas.classList.remove('hidden')
+  }
+
+  const hideCanvas = () => {
+    canvas.classList.add('hidden')
+    video.classList.remove('hidden')
+  }
+
   const clearCanvas = () => {
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     setClearCanvas(true)
-    canvas.classList.add('hidden')
-    video.classList.remove('hidden')
+    hideCanvas()
   }
 
   const killStreamAndCloseModal = () => {
     const killStream = () => {
       if (video) {
         video.srcObject = null
+        /* Stop stream */
         stream.getTracks().forEach((track) => track.stop())
       }
     }
+
     setSelectedFile(null)
     clearCanvas()
     killStream()
     onRequestClose()
-  }
-
-  useEffect(() => {
-    (async () => {
-      if (isOpen) {
-        /* Create a stream and assign it to the src of our video element */
-        /* Requesting User Permission */
-        const currentStream = await navigator.mediaDevices.getUserMedia({ video: true })
-        setStream(currentStream)
-        video.srcObject = currentStream
-      }
-    })()
-  }, [isOpen])
-
-  const handleTakePicture = async () => {
-    const convertImgDataUrlIntoFile = async (imageDataUrl) => (
-      fetch(imageDataUrl)
-        .then((it) => it.blob())
-        .then((blob) => new File(
-          [blob],
-          'newPicture.jpg',
-          {
-            type: 'image/jpeg',
-            lastModified: new Date(),
-          },
-        ))
-    )
-    const drawAndGetCanvasImageUrl = () => {
-      video.classList.add('hidden')
-      canvas.classList.remove('hidden')
-      canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
-      setClearCanvas(false)
-      return canvas.toDataURL('image/*')
-    }
-    const imageDataUrl = drawAndGetCanvasImageUrl()
-    // Check file size before setting
-    setSelectedFile(
-      await convertImgDataUrlIntoFile(imageDataUrl),
-    )
-    pauseStream()
   }
 
   const handleTryAgain = () => {
@@ -97,45 +80,93 @@ const UseCameraModal = forwardRef(({
     resumeStream()
   }
 
+  const handleTakePicture = async () => {
+    /* Draw an image - of same width and height */
+    /* as canvas - from the current stream      */
+    const drawImageOnCanvas = () => {
+      showCanvas()
+      canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
+      setClearCanvas(false)
+    }
+
+    const getCanvasImageUrl = () => canvas.toDataURL('image/jpeg')
+
+    /* Returns a chained promise that resolves into an image file. */
+    const convertImgDataUrlIntoFile = async (imageDataUrl) => (
+      fetch(imageDataUrl)
+        .then((it) => it.blob())
+        .then((blob) => new File(
+          [blob],
+          'newPicture.jpeg',
+          {
+            type: 'image/jpeg',
+            lastModified: new Date(),
+          },
+        ))
+    )
+
+    /* Start the process of taking a picture ===> */
+    pauseStream()
+    drawImageOnCanvas()
+    const imageDataUrl = getCanvasImageUrl()
+    const imageFile = await convertImgDataUrlIntoFile(imageDataUrl)
+
+    /* Check file size before setting file */
+    if (imageFile.size > 2048000) {
+      alert('Image size must not exceed 2MB')
+      handleTryAgain()
+    } else {
+      setSelectedFile(imageFile)
+    }
+  }
+
+  const handleSubmit = () => {
+    formHandler(selectedFile)
+    killStreamAndCloseModal()
+  }
+
   return (
     <dialog
       ref={ref}
-      className={`${styles.imageModal} ${styles['use-camera-modal']}`}
+      className={styles.imageModal}
     >
-      <Button text="X" onClick={() => killStreamAndCloseModal()} />
+      <div className={styles.imageModal__heading}>
+        <Button text="x" onClick={() => killStreamAndCloseModal()} />
+      </div>
+
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <video id="video" width="320" height="240" autoPlay />
-      <canvas id="canvas" className="hidden" width="320" height="240" />
+      <video id="video" autoPlay />
+      <canvas id="canvas" className="hidden" />
 
-      {isClearCanvas
-        && (
-          <Button
-            text="Take Photo"
-            onClick={handleTakePicture}
-          />
-        )}
-
-      {!isClearCanvas
-        && (
-          <>
+      <div className={styles['imageModal__btn-container']}>
+        {isClearCanvas
+          && (
             <Button
-              text="Try again"
-              onClick={handleTryAgain}
+              text="Take Photo"
+              onClick={handleTakePicture}
             />
-            <form
-              method="dialog"
-              onSubmit={() => {
-                formHandler(selectedFile)
-                killStreamAndCloseModal()
-              }}
-            >
-              <ConfirmButton
-                text={confirmButtonText}
-                disabled={selectedFile === null}
+          )}
+
+        {/* If a picture was taken and drawn,
+            display try again and save buttons */}
+        {!isClearCanvas
+          && (
+            <>
+              <Button
+                text="Try again"
+                onClick={handleTryAgain}
               />
-            </form>
-          </>
-        )}
+              <form
+                method="dialog"
+                onSubmit={handleSubmit}
+              >
+                <ConfirmButton
+                  text={confirmButtonText}
+                />
+              </form>
+            </>
+          )}
+      </div>
     </dialog>
   )
 })
