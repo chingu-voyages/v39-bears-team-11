4bb272dba6e607d390f8960b0d8a4500912cc073
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-05-26 18:23:12 +0100

    Move services inside src and refactor messagesService

--- client/src/services/messages.js
+++ client/src/services/messages.js
@@ -1,30 +1,26 @@
 import axios from 'axios'
+import axiosConfigObject from '../utils/config'
 
 const baseUrl = process.env.REACT_APP_BASE_MESSAGES_URL
 
 const getAll = async (id, token) => {
-  const config = {
-    headers: { Authorization: `bearer ${token}` },
-  }
+  const config = axiosConfigObject(token)
+
   const response = await axios.get(`${baseUrl}/${id}`, config)
   return response.data
 }
 
 const createTextMessage = async (textMessageObject, token) => {
-  const config = {
-    headers: { Authorization: `bearer ${token}` },
-  }
+  const config = axiosConfigObject(token)
+
   const response = await axios.post(baseUrl, textMessageObject, config)
   return response.data
 }
 
 const createPictureMessage = async (pictureMessageformData, token) => {
-  const config = {
-    headers: {
-      Authorization: `bearer ${token}`,
-      ContentType: 'multipart/form-data',
-    },
-  }
+  const contentType = 'multipart/form-data'
+  const config = axiosConfigObject(token, contentType)
+
   const response = await axios.post(baseUrl, pictureMessageformData, config)
   return response.data
 }
