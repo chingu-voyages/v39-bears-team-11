35f1bb904a52f8037c45d21f5a68a142cda5bb02
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-05-27 14:09:18 +0100

    emulate signing up with dummy data

--- client/src/components/chats/Chats.js
+++ client/src/components/chats/Chats.js
@@ -67,58 +67,65 @@ function Chats() {
     navigate('/chatroom')
   }
 
+  // Check if the contact has friends or chats to start with
+  if (friends && chatsData) {
   // Get the list of the latest messages, one message per contact
-  const latestChats = chatsData.reduce(
-    (newArr, currentChat) => {
+    const latestChats = chatsData.reduce(
+      (newArr, currentChat) => {
       //  Use provided friend id (from user messages) to get the friend from the friends data
-      const friendId = currentChat.id
-      const friend = friends.find((contact) => contact.id.toString() === friendId)
-      //  Use friend (from user friends array) to get friend's picture and username
-      const messagePicture = friend.picture
-      const messageUsername = friend.username
-      //  Use all the messages from the current friend
-      const allMessages = currentChat.messages
-      //  Use helper function to sort these messages and return only the latest message
-      //  sortArrayOfObjects arguments:
-      //     -array of objects
-      //     -object's property which the sorting will be based on
-      //     -choice of either ascending or descending order
-      //     -choice of returning either first item, last item, or the whole array
-      const latestMessage = sortArrayOfObjects(allMessages, 'timestamp', 'desc', 'firstItem')
-      // Use the latest message to get the timestamp and the message content
-      const messageContent = latestMessage.content
-      const messageTimestamp = latestMessage.timestamp
-      // Define new object with all prepared data
-      const newInstance = {
-        id: friendId,
-        picture: messagePicture,
-        username: messageUsername,
-        message: messageContent,
-        timestamp: messageTimestamp,
-      }
-      // Add the above object to the list of objects that is being created in this reduce function
-      newArr.push(newInstance)
-      return newArr
-    },
-    [],
-  )
-  // Sort the list of the latest chats
-  const latestChatsSorted = sortArrayOfObjects(latestChats, 'timestamp', 'desc', 'array')
+        const friendId = currentChat.id
+        const friend = friends.find((contact) => contact.id.toString() === friendId)
+        //  Use friend (from user friends array) to get friend's picture and username
+        const messagePicture = friend.picture
+        const messageUsername = friend.username
+        //  Use all the messages from the current friend
+        const allMessages = currentChat.messages
+        //  Use helper function to sort these messages and return only the latest message
+        //  sortArrayOfObjects arguments:
+        //     -array of objects
+        //     -object's property which the sorting will be based on
+        //     -choice of either ascending or descending order
+        //     -choice of returning either first item, last item, or the whole array
+        const latestMessage = sortArrayOfObjects(allMessages, 'timestamp', 'desc', 'firstItem')
+        // Use the latest message to get the timestamp and the message content
+        const messageContent = latestMessage.content
+        const messageTimestamp = latestMessage.timestamp
+        // Define new object with all prepared data
+        const newInstance = {
+          id: friendId,
+          picture: messagePicture,
+          username: messageUsername,
+          message: messageContent,
+          timestamp: messageTimestamp,
+        }
+        // Add the above object to the list of objects that is being created in this reduce function
+        newArr.push(newInstance)
+        return newArr
+      },
+      [],
+    )
+    // Sort the list of the latest chats
+    const latestChatsSorted = sortArrayOfObjects(latestChats, 'timestamp', 'desc', 'array')
+  }
 
-  return (
-    <div id="chats" className="container main">
-      <StartAChatButton handleOnClick={onOpenModal} />
-      <ModalStartAChat
-        friends={friends}
-        ref={modalRef}
-        onCloseModal={onCloseModal}
-        onOpenAChatClick={onOpenAChatClick}
-      />
-      <LatestChats
-        chats={latestChatsSorted}
-        onOpenAChatClick={onOpenAChatClick}
-      />
-    </div>
+  if (friends && chatsData) {
+    return (
+      <div id="chats" className="container main">
+        <StartAChatButton handleOnClick={onOpenModal} />
+        <ModalStartAChat
+          friends={friends}
+          ref={modalRef}
+          onCloseModal={onCloseModal}
+          onOpenAChatClick={onOpenAChatClick}
+        />
+        <LatestChats
+          chats={latestChatsSorted}
+          onOpenAChatClick={onOpenAChatClick}
+        />
+      </div>
+    )
+  } return (
+    <div>Temporary message: Add contacts first to start a chat.</div>
   )
 }
 
--- client/src/store/features/users/usersSlice.js
+++ client/src/store/features/users/usersSlice.js
@@ -6,35 +6,12 @@ import * as userService from '../../../services/userService'
 
 /* dummy user state */
 const initialState = {
-  id: '61cdd39a5a14f24e4f2f89c7',
-  // username: 'adalovelace',
-  username: false,
-  email: 'adalove@gamilcom',
-  token:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyN2IwMTFiMTkzMTllMWY0ODVhZDE2NyIsInVzZXJuYW1lIjoicnVsZXNwbGF5ZXIiLCJpYXQiOiIxNjUzMjY4MjQyIn0.jkhHC3SQp8Yx4W4VgHY1JMdrAmqQ4HirXuOzt26Sqw4',
+  id: '',
+  username: '',
+  email: '',
+  token: '',
   picture: null,
-  friends: [
-    {
-      id: 3,
-      username: 'jredmille2',
-      picture: dummyWomanPic,
-    },
-    {
-      id: 6,
-      username: 'gleband5',
-      picture: dummyWomanPic,
-    },
-    {
-      id: 33,
-      username: 'marymille',
-      picture: dummyWomanPic,
-    },
-    {
-      id: 66,
-      username: 'bandwidth7',
-      picture: dummyWomanPic,
-    },
-  ],
+  friends: null,
   isOnline: false,
 }
 
@@ -46,6 +23,7 @@ export const usersSlice = createSlice({
       ...state,
       id: action.payload.id,
       username: action.payload.username,
+      token: action.payload.token,
       isOnline: true,
     }),
     logout: (state) => ({
@@ -209,9 +187,25 @@ export function signUpUser({ username, email, password }) {
   return async (dispatch) => {
     try {
       // make the fetch request using the appropriate userService function
-      const response = await userService.signUp(username, email, password)
-      // under construction:
-      // dispatch(login(response.username, response.email, response.token))
+      // const response = await userService.signUp(username, email, password)
+
+      // The successfuly user object returned from the server contains
+      // the username, email, id, picture, contacts, token, refreshToken, and timestamps.
+      /// This is a mock returned data
+      const returnedUserObject = {
+        id: '61cdd39a5a14f24e4f2f89c7',
+        username,
+        email,
+        token:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyN2IwMTFiMTkzMTllMWY0ODVhZDE2NyIsInVzZXJuYW1lIjoicnVsZXNwbGF5ZXIiLCJpYXQiOiIxNjUzMjY4MjQyIn0.jkhHC3SQp8Yx4W4VgHY1JMdrAmqQ4HirXuOzt26Sqw4',
+        picture: null,
+        refreshToken: '12312fgfsdg',
+        timestamp: 1653655303,
+        friends: [],
+      }
+      console.log(returnedUserObject)
+
+      dispatch(login(returnedUserObject))
     } catch (error) {
       // handle error
       console.log(error.message)
