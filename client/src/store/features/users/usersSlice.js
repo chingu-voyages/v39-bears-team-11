c73bbd024ab630c33d09f814f4017a8201ef0939
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-05-27 21:24:49 +0100

    fix typo

--- client/src/store/features/users/usersSlice.js
+++ client/src/store/features/users/usersSlice.js
@@ -23,7 +23,7 @@ export const usersSlice = createSlice({
       ...state,
       id: action.payload.id,
       username: action.payload.username,
-      email: action.payload.username,
+      email: action.payload.email,
       token: action.payload.token,
       isOnline: true,
     }),
