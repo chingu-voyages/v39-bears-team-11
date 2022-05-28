c5f4328f3ab677b09d84b33c778ab650d8d2fcef
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-05-28 01:57:26 +0100

    Add generateAllTokens and getTokenFromHeader helper functions

--- server/utils/helper.js
+++ server/utils/helper.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+
 // helper function to create error object
 const errorConfig = (
   type = 'ValidationError',
@@ -13,4 +15,46 @@ const errorConfig = (
   return error
 }
 
-module.exports = errorConfig
+const generateAllTokens = (user) => {
+  /* Tokens are created by encrypting three user */
+  /* fields - username, email, id - and a secret */
+  const userForToken = {
+    id: user.id,
+    username: user.username,
+    email: user.email,
+  }
+
+  /* Access token expires every 15 minutes while */
+  /* the refresh token has no expiration date.   */
+  const token = jwt.sign(
+    userForToken,
+    process.env.TOKEN_SECRET,
+    { expiresIn: 60 * 15 }
+  )
+
+  const refreshToken = jwt.sign(
+    userForToken,
+    process.env.REFRESH_TOKEN_SECRET,
+  )
+
+  return {
+    token,
+    refreshToken,
+  }
+}
+
+/* Parse authorization header to extract token */
+const getTokenFromHeader =  (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+module.exports = {
+  errorConfig,
+  generateAllTokens,
+  getTokenFromHeader,
+}
+
