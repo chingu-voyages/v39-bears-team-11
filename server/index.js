9604ee8e87b77ed80f0d6e709a369b62916e272b
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-05-30 16:07:34 +0100

    Create a socket-io instance and attach http server to it

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const http = require('http')
 const app = require('./app')
+require('dotenv').config()
 const { infoLogger } = require('./utils/logger')
 const { PORT } = require('./utils/config')
+const SetSocketServer = require('./socket/index')
 
 const server = http.createServer(app)
+
+/* Create a socket.io instance */
+const io = require('socket.io')(server, {
+  maxHttpBufferSize: 2e6,
+  cors: {
+    origin: process.env.CLIENT_APP_URL,
+  },
+})
+
+/* Initialize the socket server */
+SetSocketServer(io)
 
 server.listen(PORT, () => {
   infoLogger(`Server running on port ${PORT}`)
