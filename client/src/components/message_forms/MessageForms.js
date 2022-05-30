71ffb92af4a3d58713de3132eadce984e16f7b74
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-05-30 18:10:31 +0100

    Add communication with socket server to messageForms
    Create a socket-io-client instance
    Add socket event handlers: welcome, getMessage, getUsers
    Add socket event dispatchers: addUser, sendMessage
    Add sendMessagetoSocket function to submit handlers in NewMessageForm and ImageModal

--- client/src/components/message_forms/MessageForms.js
+++ client/src/components/message_forms/MessageForms.js
@@ -1,24 +1,60 @@
-import { useRef } from 'react'
-import { useDispatch } from 'react-redux'
+import { useRef, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { io } from 'socket.io-client'
 import styles from '../../styles/ChatRoom-styles/MessageForms.module.css'
 import NewMessageForm from '../new_message_form/NewMessageForm'
 import NewPictureButton from '../button/NewPictureButton'
 import defaultPicture from '../../icons/img-file-placeholder.png'
 import ImageModal from '../modal/ImageModal'
-import { addPictureMessage } from '../../store/features/messages/messagesSlice'
+import { addPictureMessage, appendMessage } from '../../store/features/messages/messagesSlice'
 
 function MessageForms() {
+  const userState = useSelector(({ user }) => user)
+  const currentFriendId = useSelector(({ messages }) => messages.currentFriendId)
+  const socket = useRef()
   const imageModalRef = useRef()
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    socket.current = io(process.env.REACT_APP_SOCKET_URL)
+    socket.current.on('welcome', (message) => {
+      console.log(message)
+    })
+    socket.current.on('getMessage', ({ senderId, message }) => (
+      senderId === currentFriendId
+      && dispatch(appendMessage({
+        sender: senderId,
+        receiver: userState.id,
+        content: message,
+        timestamp: new Date().toISOString(),
+      }))
+    ))
+  }, [])
+
+  useEffect(() => {
+    socket.current.emit('addUser', userState.id)
+    socket.current.on('getUsers', (users) => {
+      console.log(users)
+    })
+  }, [userState])
+
+  const sendMessagetoSocket = (message) => {
+    socket.current.emit('sendMessage', ({
+      senderId: userState.id,
+      receiverId: currentFriendId,
+      message,
+    }))
+  }
 
   const SendPictureFormHandler = (imgFile) => {
     console.log(imgFile)
     dispatch(addPictureMessage(imgFile))
+    sendMessagetoSocket(imgFile)
   }
 
   return (
     <div className={styles['message-forms']}>
-      <NewMessageForm />
+      <NewMessageForm socketSendHandler={sendMessagetoSocket} />
       <NewPictureButton clickHandler={() => imageModalRef.current.showModal()} />
 
       <ImageModal
