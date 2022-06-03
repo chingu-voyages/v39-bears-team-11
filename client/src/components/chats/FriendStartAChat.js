7486458099e2c057d66d530f0ba32ebafdbe047b
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-06-04 00:28:22 +0100

    Add default image to SearchResult, LatestChat, and FriendStartAChat components
    Refactor LatestChat to handle the case of an image latest message
    Modify LatestChat timestamp value to match the modifications in Chats component

--- client/src/components/chats/FriendStartAChat.js
+++ client/src/components/chats/FriendStartAChat.js
@@ -1,4 +1,5 @@
 import styles from '../../styles/FriendsStartAChat.module.css'
+import defaultPicture from '../../icons/default-user-profile-image.png'
 
 function FriendStartAChat({
   username, picture, id, onOpenAChatClick,
@@ -6,7 +7,7 @@ function FriendStartAChat({
   return (
     <button type="submit" className={styles.friend} onClick={() => onOpenAChatClick(id)}>
       <div className={styles.friend__image}>
-        <img src={picture} alt="profile" />
+        <img src={picture || defaultPicture} alt="profile" />
       </div>
       <span className={styles.friend__username}>{username}</span>
 
--- client/src/components/chats/LatestChat.js
+++ client/src/components/chats/LatestChat.js
@@ -1,20 +1,29 @@
 import Moment from 'react-moment'
 import styles from '../../styles/LatestChat.module.css'
+import defaultPicture from '../../icons/default-user-profile-image.png'
 
 function LatestChat({
   username, picture, message, timestamp, id, onOpenAChatClick,
 }) {
+  const showContent = () => (
+    typeof message === 'string'
+      ? message
+      : 'image file'
+  )
+
   return (
     <button type="button" className={styles['latest-chat']} onClick={() => onOpenAChatClick(id)}>
       <div className={styles['latest-chat__image']}>
-        <img src={picture} alt="profile" />
+        <img src={picture || defaultPicture} alt="profile" />
       </div>
       <div className={styles['latest-chat__username-and-message-container']}>
         <span className={styles['latest-chat__username']}>{username}</span>
-        <span className={styles['latest-chat__message']}>{message}</span>
+        <span className={styles['latest-chat__message']}>
+          {showContent()}
+        </span>
       </div>
       <span className={styles['latest-chat__time']}>
-        <Moment unix fromNow>{timestamp}</Moment>
+        <Moment unix fromNow>{Math.floor(timestamp.getTime() / 1000)}</Moment>
       </span>
     </button>
   )
