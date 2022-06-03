48c75a16c7cea2d07ec04ce7f6cc46a3d6b40d5a
KhadidjaArezki <khadidjaarezki999@gmail.com>
2022-06-03 23:58:38 +0100

    Remove dummy data from store and update thunks to use getState to pass data to services

--- client/src/store/features/messages/messagesSlice.js
+++ client/src/store/features/messages/messagesSlice.js
@@ -1,46 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 import messagesService from '../../../services/messages'
 
-/* Dummy messages data */
 const initialState = {
-  currentFriendId: 3,
-  chats: [
-    {
-      friendId: 3,
-      messages: [
-        {
-          sender: 3,
-          receiver: 9999,
-          content: 'I want those files from you. I want you to send 1 PDF and 1 image file',
-          timestamp: '2022-03-30T15:28:18.792+00:00',
-        },
-        {
-          sender: 9999,
-          receiver: 3,
-          content: 'I\'ve found some cool photos for our travel app. Are you interested ?',
-          timestamp: '2022-03-31T15:28:18.792+00:00',
-        },
-        {
-          sender: 3,
-          receiver: 9999,
-          content: 'Ok, thanks. Can you send them this evening. I need to update my content as soon as possible to keep things intersting.',
-          timestamp: '2022-04-01T15:28:18.792+00:00',
-        },
-        {
-          sender: 3,
-          receiver: 9999,
-          content: 'Ok, thanks. Can you send them this evening. I need to update my content as soon as possible to keep things intersting.',
-          timestamp: '2022-04-01T15:28:18.792+00:00',
-        },
-        {
-          sender: 9999,
-          receiver: 3,
-          content: 'I\'ve found some cool photos for our travel app.',
-          timestamp: '2022-04-10T15:28:18.792+00:00',
-        },
-      ],
-    },
-  ],
+  currentFriendId: '',
+  chats: [],
 }
 
 export const messagesSlice = createSlice({
@@ -78,14 +41,18 @@ export function addTextMessage(textMessage) {
   return async (dispatch, getState) => {
     /* Send post request to messagesService */
     /* Replace currentFriendId with this dymmy id to test '6273ca3282ed5c0d4694857f' */
-    const { token } = getState().user
+    const { token, refreshToken } = getState().user
     const textMessageObject = {
       userId: getState().user.id,
       friendId: getState().messages.currentFriendId,
       content: textMessage,
       timestamp: new Date().toISOString(),
     }
-    const createdMessage = await messagesService.createTextMessage(textMessageObject, token)
+    const createdMessage = await messagesService.createTextMessage(
+      textMessageObject,
+      token,
+      refreshToken,
+    )
     dispatch(appendMessage(createdMessage))
 
     /* uncomment the following code and comment out the above code to test frontend only */
@@ -101,7 +68,7 @@ export function addTextMessage(textMessage) {
 export function addPictureMessage(pictureMessage) {
   return async (dispatch, getState) => {
     /* Send post request to messagesService */
-    const { token } = getState().user
+    const { token, refreshToken } = getState().user
 
     /* Replace currentFriendId with this dymmy id to test '6273ca3282ed5c0d4694857f' */
     const formData = new FormData()
@@ -110,7 +77,7 @@ export function addPictureMessage(pictureMessage) {
     formData.append('image', pictureMessage)
     formData.append('timestamp', new Date().toISOString())
 
-    const createdMessage = await messagesService.createPictureMessage(formData, token)
+    const createdMessage = await messagesService.createPictureMessage(formData, token, refreshToken)
     console.log(createdMessage)
     dispatch(appendMessage(createdMessage))
 
